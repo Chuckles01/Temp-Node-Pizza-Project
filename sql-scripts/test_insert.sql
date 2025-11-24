2f3784a05a54b70763520de233624255c006fb85
Harrison Ingram-Bate <harrison.ingrambate@gmail.com>
2025-11-23 18:46:23 -0800

    added the previously missing test_insert.sql file

---
+++ sql-scripts/test_insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO recipe (recipe_name, flavor, texture, type, time)
+VALUES ('sour chewy squares', 'sour', 'chewy', 'snack', '15min');
+
+INSERT INTO recipe (recipe_name, flavor, texture, type, time)
+VALUES ('recipe with milk and fish', 'sour', 'chewy', 'snack', '15min');
+INSERT INTO common_allergen VALUES (2, 'milk');
+INSERT INTO common_allergen VALUES (2, 'fish');
