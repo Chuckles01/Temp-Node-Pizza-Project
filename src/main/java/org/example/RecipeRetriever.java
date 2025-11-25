c11ae4b7f9ef7248d738b1e4d1880153e6b5b029
Harrison Ingram-Bate <harrison.ingrambate@gmail.com>
2025-11-24 20:57:52 -0800

    added RecipeRetriever which includes a method to that returns a string of the stored recipe from the database

---
+++ src/main/java/org/example/RecipeRetriever.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.sql.SQLException;
+import java.sql.*;
+
+public class RecipeRetriever {
+    public String getRecipe(int recipe_id) {
+        String recipe_string = "";
+
+        try{
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/mealers_choice",
+                    "root",
+                    "password"
+            );
+
+            // query to get recipe_lines in order
+            Statement statement = connection.createStatement();
+            String query = "SELECT line" +
+                    " FROM recipe_line" +
+                    " WHERE recipe_id = " + recipe_id +
+                    " ORDER BY recipe_line" +
+                    ";";
+
+            ResultSet resultSet = statement.executeQuery(query);
+
+            // constructs a single string from the retrieved lines
+            while (resultSet.next()) {
+                recipe_string = resultSet.getString("line");
+            }
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+        return recipe_string;
+    }
+}
