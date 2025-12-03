b462194512d96bf5b5dd3f7f2a77d50356c72ab2
Harrison Ingram-Bate <harrison.ingrambate@gmail.com>
2025-12-02 17:10:10 -0800

    created insert_script.sql containing only information related to recipes not the recipe_lines yet

---
+++ sql-scripts/insert_script.sql
@@ -0,0 +1,78 @@
+INSERT INTO recipe (recipe_id, recipe_name, flavor, texture, type, time)
+VALUES (1, 'BTLA', 'salty', 'crispy', 'lunch', '15min');
+INSERT INTO common_allergen VALUES (1, 'wheat');
+INSERT INTO common_allergen VALUES (1, 'milk');
+INSERT INTO common_allergen VALUES (1, 'eggs');
+
+
+INSERT INTO recipe (recipe_id, recipe_name, flavor, texture, type, time)
+VALUES (2, 'Breakfast Quesadillas', 'savory', 'gooey', 'breakfast', '30min');
+INSERT INTO common_allergen VALUES (2, 'eggs');
+INSERT INTO common_allergen VALUES (2, 'milk');
+INSERT INTO common_allergen VALUES (2, 'wheat');
+
+
+INSERT INTO recipe (recipe_id, recipe_name, flavor, texture, type, time)
+VALUES (3, 'Buffalo Cauliflower Bites', 'savory', 'moist', 'appetizer', '30min');
+INSERT INTO common_allergen VALUES (3, 'milk');
+INSERT INTO common_allergen VALUES (3, 'eggs');
+INSERT INTO common_allergen VALUES (3, 'wheat');
+
+
+INSERT INTO recipe (recipe_id, recipe_name, flavor, texture, type, time)
+VALUES (4, 'Caesar Wrap', 'savory', 'tender', 'lunch', '15min');
+INSERT INTO common_allergen VALUES (4, 'milk');
+INSERT INTO common_allergen VALUES (4, 'eggs');
+INSERT INTO common_allergen VALUES (4, 'wheat');
+
+
+INSERT INTO recipe (recipe_id, recipe_name, flavor, texture, type, time)
+VALUES (5, 'Cinnamon Bars', 'sweet', 'chewy', 'dessert', '1hr30min');
+INSERT INTO common_allergen VALUES (5, 'milk');
+INSERT INTO common_allergen VALUES (5, 'eggs');
+INSERT INTO common_allergen VALUES (5, 'wheat');
+
+
+INSERT INTO recipe (recipe_id, recipe_name, flavor, texture, type, time)
+VALUES (6, 'French Toast', 'sweet', 'moist', 'breakfast', '15min');
+INSERT INTO common_allergen VALUES (6, 'wheat');
+INSERT INTO common_allergen VALUES (6, 'milk');
+INSERT INTO common_allergen VALUES (6, 'eggs');
+
+
+INSERT INTO recipe (recipe_id, recipe_name, flavor, texture, type, time)
+VALUES (7, 'Green Bean Casserole', 'salty', 'creamy', 'dinner', '45min');
+INSERT INTO common_allergen VALUES (7, 'milk');
+
+
+INSERT INTO recipe (recipe_id, recipe_name, flavor, texture, type, time)
+VALUES (8, 'New Orleans Bread Pudding', 'sweet', 'gooey', 'dessert', '1hr15min');
+INSERT INTO common_allergen VALUES (8, 'eggs');
+INSERT INTO common_allergen VALUES (8, 'wheat');
+INSERT INTO common_allergen VALUES (8, 'milk');
+
+
+INSERT INTO recipe (recipe_id, recipe_name, flavor, texture, type, time)
+VALUES (9, 'Pinwheel Sandwiches', 'salty', 'tender', 'snack', '2hr+');
+INSERT INTO common_allergen VALUES (9, 'wheat');
+INSERT INTO common_allergen VALUES (9, 'eggs');
+INSERT INTO common_allergen VALUES (9, 'milk');
+
+
+INSERT INTO recipe (recipe_id, recipe_name, flavor, texture, type, time)
+VALUES (10, 'Red Pepper Hummus', 'savory', 'creamy', 'snack', '15min');
+INSERT INTO common_allergen VALUES (10, 'sesame');
+
+
+INSERT INTO recipe (recipe_id, recipe_name, flavor, texture, type, time)
+VALUES (11, 'Spinich Puffs', 'savory', 'crispy', 'appetizer', '30min');
+INSERT INTO common_allergen VALUES (11, 'wheat');
+INSERT INTO common_allergen VALUES (11, 'eggs');
+INSERT INTO common_allergen VALUES (11, 'milk');
+
+
+INSERT INTO recipe (recipe_id, recipe_name, flavor, texture, type, time)
+VALUES (12, 'Steak Pie', 'savory', 'tender', 'dinner', '2hr');
+INSERT INTO common_allergen VALUES (12, 'wheat');
+INSERT INTO common_allergen VALUES (12, 'eggs');
+INSERT INTO common_allergen VALUES (12, 'milk');
