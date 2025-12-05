ebc43918ebbfe21f1ac06a5e2bf17494feb3121a
Harrison Ingram-Bate <harrison.ingrambate@gmail.com>
2025-12-04 19:55:48 -0800

    re-added insert_script.sql

--- sql-scripts/test_insert.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO recipe (recipe_name, flavor, texture, type, time)
-VALUES ('sour chewy squares', 'sour', 'chewy', 'snack', '15min');
-
-INSERT INTO recipe (recipe_name, flavor, texture, type, time)
-VALUES ('recipe with milk and fish', 'sour', 'chewy', 'snack', '15min');
-INSERT INTO common_allergen VALUES (2, 'milk');
-INSERT INTO common_allergen VALUES (2, 'fish');
